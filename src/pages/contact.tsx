2d331f46d633d29059b980568a9e13f865351f3b
Cory Miller <corydhmiller@gmail.com>
2020-04-16 09:34:30 -0500

    Clean up contact page to prep for template

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
 import { withSiteData } from 'react-static'
-import { Episode, FMType } from '../types'
-import Header from '@src/components/Header'
 import Footer from '@src/components/Footer'
-import ShowList from '@src/components/ShowList'
-import ShowNotes from '@src/components/ShowNotes'
+
 import styled from 'styled-components'
 
 const Main = styled('main')`
@@ -14,37 +11,12 @@ const Main = styled('main')`
   padding:0 1rem;
 `
 
-type Props = {
-  frontmatters: FMType[]
-  mostRecentEpisode: Episode
-  title: string
-  description: string
-  myURL: string
-  image: string
-}
 export default withSiteData(
-  ({
-    frontmatters,
-    mostRecentEpisode,
-    title,
-    description,
-    myURL,
-    image,
-  }: Props) => {
+  ({}) => {
     return (
       <>
-        <Header
-          siteData={{
-            title,
-            description,
-            myURL,
-            image,
-          }}
-          mostRecentEpisode={mostRecentEpisode}
-        />
+        
         <Main>
-          <ShowList frontmatters={frontmatters} />
-          <ShowNotes mostRecentEpisode={mostRecentEpisode} />
           HERRO
         </Main>
         <Footer />
