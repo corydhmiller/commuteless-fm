fde312aac5fc699f62f49aa63c497edba165e5e2
Cory Miller <corydhmiller@gmail.com>
2020-05-10 16:23:02 -0500

    Remove old helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,8 @@
 export const dateIsInFuture = (dateString: string) => {
   const dateOfBuild = new Date()
   // Because this will add hours, minutes, seconds, and milliseconds from the date of the build, we need to reset those. The only way I know how to do this is with these methods.
+  // Also TypeScript yells a lot whenever you use non-octal literals. Hence the parseInt garbage.
+  // I'm not proud of it, but hey, when was the last time you were proud of every piece of code you wrote?
   dateOfBuild.setHours(parseInt("06", 8))
   dateOfBuild.setMinutes(parseInt("00", 8))
   dateOfBuild.setSeconds(parseInt("00", 8))
@@ -9,17 +11,4 @@ export const dateIsInFuture = (dateString: string) => {
   const dateToCheck = new Date(dateString)
 
   return dateOfBuild < dateToCheck
-}
-
-export const formatDate = (datestring: string) => {
-  const date = new Date(datestring)
-  const year = date.getFullYear()
-
-  let month = (1 + date.getMonth()).toString()
-  month = month.length > 1 ? month : "0" + month
-
-  let day = date.getDate().toString()
-  day = day.length > 1 ? day : "0" + day
-
-  return month + "/" + day + "/" + year
 }
